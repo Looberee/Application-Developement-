896f2cd03e2ed49a5e67fff911da401d6fafe0a0
Your Name <you@example.com>
2023-04-06 00:02:10 +0700

    Add Category Controller

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication123.Models;
+using WebApplication123.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebApplication123.ModelsCRUD.Category;
+
+namespace WebApplication123.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public CategoryController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        [HttpGet]
+        public async Task<IActionResult> CategoryIndex()
+        {
+            var category = await context.Categories.ToListAsync();
+            return View(category);
+        }
+        [HttpGet]
+        public IActionResult CreateCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(AddCategoryViewModel CategoryModel)
+        {
+            var category = new Category()
+            {
+                Name = CategoryModel.Name,
+                Description = CategoryModel.Description
+
+
+            };
+
+            await context.Categories.AddAsync(category);
+            await context.SaveChangesAsync();
+            return RedirectToAction("CategoryIndex");
+        }
+        [HttpGet]
+        public async Task<IActionResult> ViewCategory(int id)
+        {
+            var category = await context.Categories.FirstOrDefaultAsync(x => x.CategoryId == id);
+
+            if (category != null)
+            {
+                var viewmodel = new UpdateCategoryView()
+                {
+                    CategoryId = category.CategoryId,
+                    Name = category.Name,
+                    Description = category.Description
+                };
+
+                return await Task.Run(() => View("ViewCategory", viewmodel));
+            }
+
+            return RedirectToAction("CategoryIndex");
+        }
+        [HttpPost]
+        public async Task<IActionResult> ViewCategory(UpdateCategoryView model)
+        {
+            var category = await context.Categories.FindAsync(model.CategoryId);
+            if (category != null)
+            {
+                category.Name = model.Name;
+                category.Description = model.Description;
+
+                await context.SaveChangesAsync();
+
+                return RedirectToAction("CategoryIndex");
+            }
+
+            return RedirectToAction("CategoryIndex");
+        }
+        [HttpPost]
+        public async Task<IActionResult> DeleteCategory(UpdateCategoryView model)
+        {
+            var category = await context.Categories.FindAsync(model.CategoryId);
+
+            if (category != null)
+            {
+                context.Categories.Remove(category);
+
+
+                await context.SaveChangesAsync();
+
+                return RedirectToAction("CategoryIndex");
+            }
+
+            return RedirectToAction("CategoryIndex");
+        }
+    }
+}
